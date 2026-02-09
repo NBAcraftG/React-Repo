c5d556d3330ddb92dee7c7398ef33a18923818b5
Kurt Biñas <kcbinas@student.apc.edu.ph>
2026-02-09 10:51:47 +0000

    Tried out react

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Greetings! This is NBAcraft.</h1>
+      <h3>If you are reading this, Have a nice day!</h3>
+    </div>
+  );
+}
+
+export default App;
